8e18b52b57c1cd145ed5080d0dd67a4a9c3cae1d
Calvin Huang <c@lvin.me>
2017-01-09 15:27:21 +0530

    some tests

---
+++ test/test_client.js
@@ -0,0 +1,159 @@
+/* eslint-env mocha */
+'use strict';
+var scaleapi = require('../lib/scaleapi');
+var chai = require('chai');
+var expect = chai.expect;
+var test_api_key = process.env.SCALE_TEST_API_KEY;
+
+expect(test_api_key).to.not.be.a('null');
+
+var client = new scaleapi.ScaleClient({apikey: test_api_key});
+
+describe('task creation', () => {
+  it('categorize', done => {
+    client.create_categorization_task({
+      callback_url: 'http://www.example.com/callback',
+      instruction: 'Is this company public or private?',
+      attachment_type: 'website',
+      attachment: 'http://www.google.com/',
+      categories: ['public', 'private']}, done);
+  });
+
+  it('categorize fail', done => {
+    client.create_categorization_task({
+      callback_url: 'http://www.example.com/callback',
+      categories: ['public', 'private']},
+      err => {
+        expect(err).to.be.an.instanceof(scaleapi.ScaleException);
+        done();
+      });
+  });
+
+  it('transcription', done => {
+    client.create_transcription_task({
+      callback_url: 'http://www.example.com/callback',
+      instruction: 'Transcribe the given fields. Then for each news item on the page, transcribe the information for the row.',
+      attachment_type: 'website',
+      attachment: 'http://www.google.com/',
+      fields: {
+        'title': 'Title of Webpage',
+        'top_result': 'Title of the top result'
+      },
+      row_fields: {
+        'username': 'Username of submitter',
+        'comment_count': 'Number of comments'
+      }}, done);
+  });
+
+  it('transcription fail', done => {
+    client.create_transcription_task({
+      callback_url: 'http://www.example.com/callback',
+      attachment_type: 'website'},
+      err => {
+        expect(err).to.be.an.instanceof(scaleapi.ScaleException);
+        done();
+      });
+  });
+
+  it('phonecall', done => {
+    client.create_phonecall_task({
+      callback_url: 'http://www.example.com/callback',
+      instruction: 'Call this person and follow the script provided, recording responses',
+      phone_number: '5055006865',
+      entity_name: 'Alexandr Wang',
+      script: 'Hello ! Are you happy today? (pause) One more thing - what is your email address?',
+      fields: {'email': 'Email Address'},
+      choices: ['He is happy', 'He is not happy']}, done);
+  });
+
+  it('phonecall fail', done => {
+    client.create_phonecall_task({
+      callback_url: 'http://www.example.com/callback',
+      instruction: 'Call this person and follow the script provided, recording responses'},
+      err => {
+        expect(err).to.be.an.instanceof(scaleapi.ScaleException);
+        done();
+      });
+  });
+
+  it('comparison', done => {
+    client.create_comparison_task({
+      callback_url: 'http://www.example.com/callback',
+      instruction: 'Do the objects in these images have the same pattern?',
+      attachment_type: 'image',
+      attachments: [
+        'http://i.ebayimg.com/00/$T2eC16dHJGwFFZKjy5ZjBRfNyMC4Ig~~_32.JPG',
+        'http://images.wisegeek.com/checkered-tablecloth.jpg'
+      ],
+      choices: ['yes', 'no']}, done);
+  });
+
+  it('comparison fail', done => {
+    client.create_comparison_task({
+      callback_url: 'http://www.example.com/callback',
+      instruction: 'Do the objects in these images have the same pattern?',
+      attachment_type: 'image'},
+      err => {
+        expect(err).to.be.an.instanceof(scaleapi.ScaleException);
+        done();
+      });
+  });
+
+  it('annotation', done => {
+    client.create_annotation_task({
+      callback_url: 'http://www.example.com/callback',
+      instruction: 'Draw a box around each **baby cow** and **big cow**',
+      attachment_type: 'image',
+      attachment: 'http://i.imgur.com/v4cBreD.jpg',
+      objects_to_annotate: ['baby cow', 'big cow'],
+      with_labels: true}, done);
+  });
+
+  it('annotation fail', done => {
+    client.create_annotation_task({
+      callback_url: 'http://www.example.com/callback',
+      instruction: 'Draw a box around each **baby cow** and **big cow**',
+      attachment_type: 'image'},
+      err => {
+        expect(err).to.be.an.instanceof(scaleapi.ScaleException);
+        done();
+      });
+  });
+
+  it('datacollection', done => {
+    client.create_datacollection_task({
+      callback_url: 'http://www.example.com/callback',
+      instruction: 'Find the URL for the hiring page for the company with attached website.',
+      attachment_type: 'website',
+      attachment: 'http://www.google.com/',
+      fields: { 'hiring_page': 'Hiring Page URL' }}, done);
+  });
+
+  it('datacollection fail', done => {
+    client.create_datacollection_task({
+      callback_url: 'http://www.example.com/callback',
+      attachment_type: 'website'},
+      err => {
+        expect(err).to.be.an.instanceof(scaleapi.ScaleException);
+        done();
+      });
+  });
+
+  it('audiotranscription', done => {
+    client.create_audiotranscription_task({
+      callback_url: 'http://www.example.com/callback',
+      attachment_type: 'audio',
+      attachment: 'https://storage.googleapis.com/deepmind-media/pixie/knowing-what-to-say/second-list/speaker-3.wav',
+      verbatim: false}, done);
+  });
+
+  it('audiotranscription fail', done => {
+    client.create_audiotranscription_task({
+      callback_url: 'http://www.example.com/callback',
+      attachment_type: 'audio'},
+      err => {
+        expect(err).to.be.an.instanceof(scaleapi.ScaleException);
+        done();
+      });
+  });
+});
